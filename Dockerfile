48416fb62411e8f2ef91f958d9dac3092c67cd8d
peihong.chen@transwarp.io <peihong.chen@transwarp.io>
2020-11-03 23:07:16 +0800

    fix: fix endpoint bugs

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM openjdk:8u171-alpine
 ADD ./schema/*.csv /run/schemas/
 ADD ./target/legacy-0.0.1-SNAPSHOT.jar /run/
 
-EXPOSE 8080
+EXPOSE 9000 9001 9002 9003
 
 ENTRYPOINT ["java", "-jar", "/run/legacy-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/apsh/legacy/util/SchemaUtil.java
+++ src/main/java/com/apsh/legacy/util/SchemaUtil.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class SchemaUtil {
 
-    private static final String schemaPath = "/Users/peihongchen/Documents/面向互联网+的软件开发技术方向综合实践/APS-H/LegacySystem/schema";
+    private static final String schemaPath = "/run/schemas";
 
     public static File loadSchema(String file) {
         return new File(schemaPath, file);
